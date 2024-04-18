a03924e5f62d9a0bc783e11d08c0df7307715f6d
bxrhhy <535495438@qq.com>
2024-04-18 16:01:34 +0800

    update dev email

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,13 @@ defineOptions({
 </script>
 
 <template>
-  <div>
+  <div relative min-h-100vh>
     <img h-full w-full src="../assets/bg.png" alt="">
+
+    <div class="email" absolute bottom-10 w-full flex justify-center>
+      <div rd-2 bg-transparent p-2 p-x-5 text-8 color-white>
+        grazianomenaker40@gmail.com
+      </div>
+    </div>
   </div>
 </template>
